4e93519b3ffa720f5d3ab0c97c17f4419fb68b09
MogilnyLeon <2362363@johnabbottcollege.net>
2024-12-11 19:19:24 -0500

    My Will to live is gone but at least the stuff I changed should work now :/
    Fixed some stuff for index.html
    mainscript.js actually receives the information and processes it to CharacterSelection.js
    
    CharacterSelection.js gets player info and dynamically changes the character preview for player 1 and 2
    TODO:
    -API calls and setting up Character class
    -Loading a skin for given character (using dynamically changing tag id, already implemented)
    -Slider to select game length
    -Submit the info and pass it gameplay script

--- scripts/CharacterSelectionLogic.js
+++ scripts/CharacterSelectionLogic.js
@@ -1,5 +1,5 @@
-//import { Player } from "../classes/player"; 
-//import { Character } from "../classes/character";
+// import { Player } from "./../classes/player"; 
+// import { Character } from "./../classes/character";
 
 //import { Player } from "../classes/player";
 
@@ -7,48 +7,57 @@
  * List of charcater sprites IDs
  *
  * The Serpent
- *  1-  374.335                     Fuzzy!
- *  2-  342.24                      Rayquaza Snek
- *  3-  148.335                     Caduceus
+ *  1-  374.335                     Fuzzy!          0
+ *  2-  342.24                      Rayquaza Snek   1
+ *  3-  148.335                     Caduceus        2
  * The Madness
- *  1-  20.68                       Muscle Rat
- *  2-  268.68 -> 268.68a           Fiery Rage
- *  3-  212.68                      Scissor Barb
+ *  1-  20.68                       Muscle Rat      0
+ *  2-  268.68 -> 268.68a           Fiery Rage      1
+ *  3-  212.68                      Scissor Barb    2
  * The Righteous
- *  1-  329.59                      Sword Doggo
- *  2-  329.369                     Fallen Knight
- *  3-  329.212 -> 329.212c         Chosen One
+ *  1-  329.59                      Sword Doggo     0
+ *  2-  329.369                     Fallen Knight   1
+ *  3-  329.212 -> 329.212c         Chosen One      2
  * The Tinkerer
- *  1-  9.6                         Steam Power!
- *  2-  337.293                     Cog Handler
- *  3-  337.439                     The Clockwork
+ *  1-  9.6                         Steam Power!    0
+ *  2-  337.293                     Cog Handler     1
+ *  3-  337.439                     The Clockwork   2
  * The Trickster
- *  1-  421.94 -> 421.94c.png       Gold Gremlin   
- *  2-  275.295 -> 275.295a         Dubious Disk
- *  3-  202.93 -> 202.93c           Eyes        
+ *  1-  421.94 -> 421.94c.png       Gold Gremlin    0
+ *  2-  275.295 -> 275.295a         Dubious Disk    1
+ *  3-  202.93 -> 202.93c           Eyes            2
  * The Sorcerer
- *  1-  38.287 -> 38.287d.png       Fire Alchemist
- *  2-  255.287 -> 255.287i.png     Witch
- *  3-  35.287                      Moon Wizard
+ *  1-  38.287 -> 38.287d.png       Fire Alchemist  0
+ *  2-  255.287 -> 255.287i.png     Witch           1
+ *  3-  35.287                      Moon Wizard     2
  * The Proficient
- *  1-  331.212 -> 331.212c.png     Steel Man
- *  2-  200.348                     Hunter
- *  3-  212.354 -> 212.354a         Punch Out
+ *  1-  331.212 -> 331.212c.png     Steel Man       0
+ *  2-  200.348                     Hunter          1
+ *  3-  212.354 -> 212.354a         Punch Out       2
  * The Darkness
- *  1-  227.287 -> 227.287a.png     Dark Lord
- *  2-  357.289                     Anomaly
- *  3-  130.295                     Ragnarok
+ *  1-  227.287 -> 227.287a.png     Dark Lord       0
+ *  2-  357.289                     Anomaly         1
+ *  3-  130.295                     Ragnarok        2
  */
  
 
+//Parse information received from index.html
+let Player1Info = sessionStorage.getItem("player1Info");
+let Player2Info = sessionStorage.getItem("player2Info");
+
+Player1Info = Player1Info.split(",");
+Player2Info = Player2Info.split(",");
 
 
+
+const Player1Name = document.getElementById("Name1");
+Player1Name.innerText = Player1Info[0];
+const Player2Name = document.getElementById("Name2");
+Player2Name.innerText = Player2Info[0];
 
 let images = document.getElementsByClassName("image");
 const body = document.getElementsByTagName("body");
 images= Array.from(images);
-console.log(images);
-populateCharcters(images)
 let previousIndex = null; 
 images.forEach((image,index) => {
     image.addEventListener("click", () => {
@@ -58,7 +67,30 @@ images.forEach((image,index) => {
         image.style.backgroundColor = "#ddd";
 
         previousIndex = index;
+        
+        let player1skin = document.querySelector(".slctChar1");
+        player1skin.src = image.src;
+        player1skin.id = `player1_${image.id}`;
+        console.log(player1skin.id);
+    });
+});
 
+let images1 = document.getElementsByClassName("image1");
+images1= Array.from(images1);
+let previousIndexAlt = null; 
+images1.forEach((image1,index) => {
+    image1.addEventListener("click", () => {
+        if (previousIndexAlt !== null && previousIndexAlt !== index) {
+            images1[previousIndexAlt].style.backgroundColor = "#F1F1F1";
+        }
+        image1.style.backgroundColor = "#ddd";
+
+        previousIndexAlt = index;
+        
+        let player2skin = document.querySelector(".slctChar2");
+        player2skin.src = image1.src;
+        player2skin.id = `player2_${image1.id}`;
+        console.log(player2skin.id);
     });
 });
 function populateCharcters(images) {
--- scripts/mainscript.js
+++ scripts/mainscript.js
@@ -1,27 +1,33 @@
-import { Player } from "../classes/player"; 
-import { Character } from "../classes/character";
-const login = document.getElementById("MainLogin");
+import { Player } from "./../classes/player.js";
 
-login.addEventListener("submit",HandleLogin);
+document.addEventListener("DOMContentLoaded", () => {
+    const login = document.getElementById("MainLogin");
+
+    login.addEventListener("submit",HandleLogin);
+})
+
 
 function HandleLogin(event) {
+
+    event.preventDefault();
 
     //Input field values
     let player1Name = document.getElementById("Name1").value;
     let player2Name = document.getElementById("Name2").value;
     let player1Age = document.getElementById("UserAge1").value;
     let player2Age = document.getElementById("UserAge2").value;
-    //Age doesn't need to be verified because it can be done through HTML
-    if(player1Name === "" || player2Name === "") {
-        alert();
+
+    if(player1Name === "" || player2Name === "" || player1Age === "" || player2Age === "") {
+        alert("Please fill out all the fields");
         event.preventDefault();
+        return;
     }
-    else {
-        //Create objects
-        const player1 = new Player(player1Name,player1Age);
-        const player2 = new Player(player2Name,player2Age);
-        //Because session storage can only handle string values, if an object os passed, it will only return [object]
-        sessionStorage.setItem("player1Info",player1.ToString);
-        sessionStorage.setItem("player2Info",player2.ToString);
-    }
+    //Create objects
+    const player1 = new Player(player1Name,player1Age);
+    const player2 = new Player(player2Name,player2Age);
+    //Because session storage can only handle string values, if an object is passed, it will only return [object]
+    sessionStorage.setItem("player1Info",player1.ToString()); //ToString() = [Name],[Age]
+    sessionStorage.setItem("player2Info",player2.ToString());
+
+    window.location.href = "./pages/character-selection.html";
 }
